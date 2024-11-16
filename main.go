afd86971bc48dd567eeea74c53f925a00bab5e30
Akmal Sindarov <asauz777@gmail.com>
2024-11-16 19:42:46 +1000

    Update main.go

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/Shikha-code36/golang-crud-rest-api-gin/database"
 	"github.com/Shikha-code36/golang-crud-rest-api-gin/handlers"
 	"github.com/gin-gonic/gin"
+	"log"
 )
 
 func main() {
@@ -17,8 +18,7 @@ func main() {
 	r.PUT("/update_user/:id", handlers.UpdateUser)
 	r.DELETE("/delete_user/:id", handlers.DeleteUser)
 
-	err := r.Run()
-	if err != nil {
-		return
+	if err := r.Run(":8080"); err != nil {
+		log.Fatal("Failed to run server:", err)
 	}
 }
